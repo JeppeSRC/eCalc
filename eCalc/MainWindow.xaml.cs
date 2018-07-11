5a12b27394df7422d204d6a7cf613c49ab76d206
JeppeSRC <jeppe97rc@gmail.com>
2018-07-11 23:13:05 +0200

    Some ui stuff

--- eCalc/MainWindow.xaml.cs
+++ eCalc/MainWindow.xaml.cs
@@ -20,6 +20,29 @@ namespace eCalc {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
+            Types[] types = new Types[(int)Types.NumTypes];
+
+            for (int i = 0; i < (int)Types.NumTypes; i++) {
+                types[i] = (Types)i;
+            }
+
+            cbType.ItemsSource = types;
+            cbType.SelectedIndex = 0;
+
+            
+        }
+
+        public enum Types {
+            OpAmp_NonInverting,
+            OpAmp_Inverting,
+            OpAmp_SchmittTrigger,
+
+            NumTypes
+        };
+
+        private void cbType_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
         }
     }
 }
