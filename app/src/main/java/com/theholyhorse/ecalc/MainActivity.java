0246e80a69ea0cc448984a9162b56ccadd0b469c
JeppeSRC <jeppe97rc@gmail.com>
2018-07-12 22:17:27 +0200

    Created project

---
+++ app/src/main/java/com/theholyhorse/ecalc/MainActivity.java
@@ -0,0 +1,24 @@
+package com.theholyhorse.ecalc;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
